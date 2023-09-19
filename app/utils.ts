b49d070082ccf3a5dabcf85f9543db917cd995ba
edison <protestmailself@gmail.com>
2023-09-19 23:00:46 +0800

    EMH: 转语音接口调用

--- app/utils.ts
+++ app/utils.ts
@@ -5,7 +5,6 @@ import Locale from "./locales";
 export function trimTopic(topic: string) {
   return topic.replace(/[，。！？”“"、,.!?]*$/, "");
 }
-
 export async function SpeechText(text: string) {
   try {
     if (window.__TAURI__) {
@@ -13,8 +12,20 @@ export async function SpeechText(text: string) {
     } else {
       await navigator.clipboard.writeText(text);
     }
-
-    showToast(Locale.Copy.Success);
+      const url = `https://api.youxiuabc.com/api/ai/speech?content=${ text }`;
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
+      xhr.send();
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+          const data = JSON.parse(xhr.responseText);
+          console.log(data);
+          const audio = new Audio(data.data);
+          audio.play();
+        }else {
+          console.log(xhr.responseText);
+        }
+      }
   } catch (error) {
     const textArea = document.createElement("textarea");
     textArea.value = text;
