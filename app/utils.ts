0a2789e8ca4103043b8ad57d08cffdfb0096837e
edison <protestmailself@gmail.com>
2023-09-28 19:37:05 +0800

    EMH: 时间自动匹配

--- app/utils.ts
+++ app/utils.ts
@@ -31,12 +31,15 @@ export async function SpeechText(text: string, i: any) {
 function scroll(str: any, j: any) {
   for (let i = 0; i < str.length; i++) {
     var msg = "";
-    setTimeout(() => {
-      msg += str[i].text + "&nbsp;";
-      document.getElementsByClassName("markdown-body")[
-        j
-      ].innerHTML = `<span style="color: red">${msg}</span>`;
-    }, str[i].end_time - str[i].begin_time);
+    setTimeout(
+      () => {
+        msg += str[i].text + "&nbsp;";
+        document.getElementsByClassName("markdown-body")[
+          j
+        ].innerHTML = `<span style="color: red">${msg}</span>`;
+      },
+      1000 + (str[i].end_time - str[i].begin_time) / 1000,
+    );
   }
 }
 export async function copyToClipboard(text: string) {
